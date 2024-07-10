1f2112d20eb48b36ffe9d4be38df825bab91b067
Денис Вершков <denis@MacBook-Pro-Denis.local>
2024-07-10 17:09:07 +0300

    add main

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
+import app from './src/index.js';
 
-console.log(_.last(['one', 'two']));
-console.log("Hello, Hexlet!");
+console.log(app());
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _  from "lodash";
+
+export default () => {
+console.log(_.last(["one", "two"]));
+console.log("Hello, Hexlet!");
+}
+
